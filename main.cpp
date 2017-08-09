deac70e8b7d99cc9ab0b47fb4e8cd2497aec2de0
Zarkane Soufiane <zarkanesoufiane@iMac-de-Laure.local>
2017-08-09 16:25:57 +0200

    Blank page simplified

--- main.cpp
+++ main.cpp
@@ -14,45 +14,33 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main(int argc, const char * argv[]) {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
     
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
     
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    }
-    else
-    {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        }
-        else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-            
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-            
-            //Wait two seconds
-            SDL_Delay( 5000 );
-        }
-    }
-    //Destroy window
-    SDL_DestroyWindow( window );
+    SDL_Init(SDL_INIT_VIDEO);
     
-    //Quit SDL subsystems
+    window = SDL_CreateWindow("My Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    
+    SDL_RenderClear(renderer);
+    
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    
+    SDL_Rect rect = { 220, 140, 200, 200 };
+    SDL_RenderFillRect(renderer, &rect);
+        
+    SDL_RenderPresent(renderer);
+    
+    SDL_Delay(2000);
+    
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    
     SDL_Quit();
-    
     return 0;
+    
 }
